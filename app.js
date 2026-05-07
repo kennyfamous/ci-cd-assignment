3843cf03423521e2295ca0b35ac36c0896d16cd1
Kennfamous <kennyogbonna22@gmail.com>
2026-05-07 03:18:45 +0100

    Updated website

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.get('/', (req, res) => {
 res.send(`
-<h1>CI/CD Assignment</h1>
+<h1>CI/CD Assignment-Deployment succesfull-Kenny-Famous</h1>
 <p>Website deployed successfully using GitHub Actions and AWS EC2.</p>
 `);
 });
