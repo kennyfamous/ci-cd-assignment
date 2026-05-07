2b9508125b5aef8295f2e5521a8b7644b64d38e0
Kennfamous <kennyogbonna22@gmail.com>
2026-05-07 01:23:20 +0100

    initial project setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+res.send(`
+<h1>CI/CD Assignment</h1>
+<p>Website deployed successfully using GitHub Actions and AWS EC2.</p>
+`);
+});
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
